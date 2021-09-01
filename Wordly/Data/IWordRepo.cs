c955dc76caa6a80b3dec86f34c0108f58d84505b
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-09-01 15:34:13 +0300

    Create the IWordRepo interface

---
+++ Wordly/Data/IWordRepo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Wordly.Models;
+
+namespace Wordly.Data
+{
+    public interface IWordRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Word> GetAllWords();
+        Word GetWordById(int id);
+        void CreateWord(Word word);
+        void UpdateWord(Word word);
+        void DeleteWord(Word word);
+    }
+}
