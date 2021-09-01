206084b2563be4043a599dc97bf21fbe6ca1bd5d
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-09-01 14:55:40 +0300

    Create Word model

---
+++ Wordly/Models/Word.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wordly.Models
+{
+    public class Word
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public string Meaning { get; set; }
+    }
+}
