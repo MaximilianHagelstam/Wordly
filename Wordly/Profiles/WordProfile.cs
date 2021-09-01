df28857c72c00818a3f6f9afeac878b8b350d84f
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-09-01 15:09:05 +0300

    Create profiles for mapping models to dtos

---
+++ Wordly/Profiles/WordProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Wordly.Dtos;
+using Wordly.Models;
+
+namespace Wordly.Profiles
+{
+    public class WordProfile : Profile
+    {
+        public WordProfile()
+        {
+            CreateMap<Word, WordReadDto>();
+            CreateMap<WordCreateDto, Word>();
+        }
+    }
+}
