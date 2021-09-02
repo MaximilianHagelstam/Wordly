7bc652c6bb03750d38931b9e999d49ea871213b4
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-09-02 10:31:25 +0300

    Fix component not found

--- Wordly/ClientApp/src/App.js
+++ Wordly/ClientApp/src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import { Route } from "react-router";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
-import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
+// import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
 import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
 import { ApplicationPaths } from "./components/api-authorization/ApiAuthorizationConstants";
 import { Words } from "./components/Words";
@@ -19,7 +17,7 @@ export default class App extends Component {
       <Layout>
         <Route exact path="/" component={Home} />
         <Route path="/words" component={Words} />
-
+        {/* <AuthorizeRoute path="/fetch-data" component={FetchData} /> */}
         <Route
           path={ApplicationPaths.ApiAuthorizationPrefix}
           component={ApiAuthorizationRoutes}
