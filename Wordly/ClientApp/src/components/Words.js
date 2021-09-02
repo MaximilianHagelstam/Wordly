6a94f64404aebd03d9c5e8204d9925cc8a3209fe
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-09-02 09:37:53 +0300

    Fetch words data in react app

---
+++ Wordly/ClientApp/src/components/Words.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import authService from "./api-authorization/AuthorizeService";
+
+export class Words extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { words: [], loading: true };
+  }
+
+  componentDidMount() {
+    this.populateWordsData();
+  }
+
+  static renderWordsTable(words) {
+    return (
+      <table className="table table-striped" aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Word</th>
+            <th>Meaning</th>
+          </tr>
+        </thead>
+        <tbody>
+          {words.map((words) => (
+            <tr key={words.id}>
+              <td>{words.body}</td>
+              <td>{words.meaning}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      Words.renderWordsTable(this.state.words)
+    );
+
+    return (
+      <div>
+        <h1 id="tabelLabel">Word</h1>
+        <p>This component demonstrates fetching words from the server.</p>
+        {contents}
+      </div>
+    );
+  }
+
+  async populateWordsData() {
+    const token = await authService.getAccessToken();
+    const response = await fetch("api/words", {
+      headers: !token ? {} : { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    this.setState({ words: data, loading: false });
+  }
+}
--- Wordly/Controllers/WeatherForecastController.cs
+++ Wordly/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace Wordly.Controllers
 {
     [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
